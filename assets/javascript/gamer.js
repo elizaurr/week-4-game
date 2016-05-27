9f1e553203178d579328689edfe1dfbfe5346e42
elizaurr <elizaurreyes@yahoo.com>
2016-05-26 22:50:53 -0400

    gamer.js add

---
+++ assets/javascript/gamer.js
@@ -0,0 +1,12 @@
+var number = math.floor((math.random() * 102) + 19);
+
+var red = math.floor((math.random() * 12) + 1);
+var blue = math.floor((math.random() * 12) + 1);
+var yellow = math.floor((math.random() * 12) + 1);
+var green = math.floor((math.random() * 12) + 1);
+
+var guessTotal = 0;
+var wins = 0;
+var losses = 0;
+var currentStreak = 0;
+var longestStreak = 0;
